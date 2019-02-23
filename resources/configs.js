dcfd686a790e2a04b793438b0cc6d7884e2e65fa
Julian Schäfer <Julian.Schaefer@gmx.de>
2019-02-23 10:20:30 +0100

    * added config jschae2s_demo

--- resources/configs.js
+++ resources/configs.js
@@ -5,6 +5,7 @@
  */
 
 const LOCAL_DATA_SERVER = "http://192.168.99.101:8080";
+const HBRS_CCM2_DATA_SERVER = "https://ccm2.inf.h-brs.de";
 
 ccm.files['configs.js'] = {
 
@@ -35,6 +36,34 @@ ccm.files['configs.js'] = {
         "teambuild_store":      ['ccm.store', {"name": "jschae2s_teambuild",         "url": LOCAL_DATA_SERVER}],
         "kanban_board_store":   ['ccm.store', {"name": "jschae2s_kanban_team_borad", "url": LOCAL_DATA_SERVER}],
         "kanban_card_store":    ['ccm.store', {"name": "jschae2s_kanban_team_cards", "url": LOCAL_DATA_SERVER}],
-    }
+    },
+
+    "jschae2s_demo": {
+        "key": "jschae2s_demo",
+        "project": "jschae2s_demo",
+
+        "user": ["ccm.instance", "https://ccmjs.github.io/akless-components/user/versions/ccm.user-8.3.1.js", {
+            "realm": "guest",
+            "guest": "jschae2s",
+            // "guest": "sschae2s",
+            // "guest": "cmann2s",
+
+            // "guest": "arindt2s",
+            // "guest": "lmuell2s",
+            // "guest": "tmann2s",
+
+            // "guest": "agersa2s",
+            // "guest": "wschae2s",
+            "title": "Guest Mode: Please enter any username"
+        }],
+        "menu": ['ccm.component', 'https://ccmjs.github.io/akless-components/menu/versions/ccm.menu-2.4.3.js'],
+        "teambuild": ['ccm.component', 'https://ccmjs.github.io/akless-components/teambuild/versions/ccm.teambuild-3.0.0.js'],
+        "kanban": ['ccm.component', '../kanban_team_board/ccm.kanban_team_board.js'],
+        "comments": ['ccm.component', 'https://ccmjs.github.io/tkless-components/comment/versions/ccm.comment-4.1.0.js'],
+
+        "teambuild_store":      ['ccm.store', {"name": "jschae2s_teambuild",         "url": HBRS_CCM2_DATA_SERVER}],
+        "kanban_board_store":   ['ccm.store', {"name": "jschae2s_kanban_team_borad", "url": HBRS_CCM2_DATA_SERVER}],
+        "kanban_card_store":    ['ccm.store', {"name": "jschae2s_kanban_team_cards", "url": HBRS_CCM2_DATA_SERVER}],
+    },
 
 };
